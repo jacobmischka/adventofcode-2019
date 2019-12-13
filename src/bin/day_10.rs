6c1ad0c027697d43e5bb7310c5edb712f8fc0da1
Jacob Mischka <jacob@mischka.me>
2019-12-13 15:47:37 -0600

    Finish initial impl, doesn't work
    How the heck do I rotate correctly on a square grid

--- src/bin/day_10.rs
+++ src/bin/day_10.rs
@@ -1,10 +1,26 @@
 use std::collections::HashSet;
+use std::convert::From;
 use std::hash::Hash;
 use std::io::{self, BufRead};
 use std::ops::Sub;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct Coord(u32, u32);
+struct Coord(i32, i32);
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Path(i32, i32);
+
+impl Path {
+    fn len(&self) -> f64 {
+        (((self.0.abs() as u32).pow(2) + (self.1.abs() as u32).pow(2)) as f64).sqrt()
+    }
+}
+
+#[test]
+fn len_works() {
+    assert_eq!(Path(1, 1).len(), 2f64.sqrt());
+    assert_eq!(Path(3, 4).len(), 5f64);
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Vector(i32, i32);
@@ -17,14 +33,33 @@ impl Vector {
     }
 }
 
-impl Sub for &Coord {
-    type Output = Vector;
+impl From<&Path> for Vector {
+    fn from(path: &Path) -> Self {
+        Self::new(path.0, path.1)
+    }
+}
 
-    fn sub(self, other: Self) -> Vector {
+impl PartialEq<Path> for Vector {
+    fn eq(&self, other: &Path) -> bool {
+        let other_vec: Vector = other.into();
+        self == &other_vec
+    }
+}
+
+#[test]
+fn vector_path_junk_works() {
+    assert_eq!(Vector::new(10, 10), Path(10, 10));
+    assert_eq!(Vector::new(1, 1), Path(10, 10));
+}
+
+impl Sub for &Coord {
+    type Output = Path;
+
+    fn sub(self, other: Self) -> Path {
         let x = self.0 as i32 - other.0 as i32;
         let y = self.1 as i32 - other.1 as i32;
 
-        Vector::new(x, y)
+        Path(x, y)
     }
 }
 
@@ -53,6 +88,10 @@ fn main() {
     let monitoring_station = coords.remove(index.unwrap());
 
     println!("Part 1: {}", max_detectable);
+
+    let vaporized = vaporize(&monitoring_station, coords);
+    let two_hundredth = &vaporized[199];
+    println!("Part 2: {}", two_hundredth.0 * 100 + two_hundredth.1);
 }
 
 fn count_detectable(coord: &Coord, coords: &[Coord]) -> u32 {
@@ -60,24 +99,58 @@ fn count_detectable(coord: &Coord, coords: &[Coord]) -> u32 {
 
     for other_coord in coords {
         if coord != other_coord {
-            set.insert(other_coord - coord);
+            set.insert((&(other_coord - coord)).into());
         }
     }
 
     set.len() as _
 }
 
-fn vaporize(laser: &Coord, asteroids: Vec<Coord>) -> Vec<Coord> {
+fn vaporize(laser: &Coord, mut asteroids: Vec<Coord>) -> Vec<Coord> {
     let mut vaporized = Vec::new();
+    let radius = 2000u32;
+
+    let mut target = Coord(laser.0, laser.1 - radius as i32);
+
+    loop {
+        let laser_vector: Vector = (&(&target - &laser)).into();
+
+        let mut min_distance = std::f64::MAX;
+        let mut asteroid_index_to_vaporize: Option<usize> = None;
+
+        for (i, asteroid) in asteroids.iter().enumerate() {
+            let a_path: Path = asteroid - laser;
+            if laser_vector == a_path {
+                let len = a_path.len();
+                if a_path.len() < min_distance {
+                    asteroid_index_to_vaporize = Some(i);
+                    min_distance = len;
+                }
+            }
+        }
+
+        if let Some(i) = asteroid_index_to_vaporize {
+            vaporized.push(asteroids.remove(i));
+            dbg!(vaporized.len(), asteroids.len());
+        }
+
+        if asteroids.is_empty() {
+            break;
+        }
+
+        target = rotate_clockwise(&target, &laser, radius);
+    }
 
     vaporized
 }
 
-fn rotate_clockwise(current: &Coord, max: &Coord) -> Coord {
+fn rotate_clockwise(current: &Coord, center: &Coord, radius: u32) -> Coord {
     let Coord(x, y) = current;
     let x = *x;
     let y = *y;
-    if y == 0 {
+    let min = Coord(center.0 - radius as i32, center.1 - radius as i32);
+    let max = Coord(center.0 + radius as i32, center.1 + radius as i32);
+    if y == min.1 {
         if x < max.0 {
             Coord(x + 1, y)
         } else {
@@ -90,14 +163,14 @@ fn rotate_clockwise(current: &Coord, max: &Coord) -> Coord {
             Coord(x - 1, y)
         }
     } else if y == max.1 {
-        if x > 0 {
+        if x > min.0 {
             Coord(x - 1, y)
         } else {
             Coord(x, y - 1)
         }
     } else {
-        // x == 0
-        if y > 0 {
+        // x == min.0
+        if y > min.1 {
             Coord(x, y - 1)
         } else {
             Coord(x + 1, y)
