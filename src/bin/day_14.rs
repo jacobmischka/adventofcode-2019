d80eac4735a9e516281aef9d3a4dc6619ce47043
Jacob Mischka <jacob@mischka.me>
2019-12-18 10:40:47 -0600

    Finish day 14
    That was actually easier than I thought it would be.

--- src/bin/day_14.rs
+++ src/bin/day_14.rs
@@ -19,50 +19,83 @@ fn main() {
 
     let fuel_reaction = reactions.get("FUEL").unwrap();
     let mut stockpile = HashMap::new();
-    let single_consumption = fuel_reaction.produce(&reactions, &mut stockpile);
+    let single_consumption = fuel_reaction.produce(1, &reactions, &mut stockpile);
 
     println!("Part 1: {}", single_consumption);
 
     let mut ore_remaining = ORE_TOTAL - single_consumption;
 
-    let mut after_one = stockpile.clone();
-    after_one.remove("FUEL");
+    let mut states: HashMap<String, (u64, u64)> = HashMap::new();
+    states.insert(
+        serialize_stockpile(&stockpile),
+        (*stockpile.get("FUEL").unwrap(), ore_remaining),
+    );
 
-    let mut cycle_consumption = 0;
+    let mut cycle_start: Option<(u64, u64)> = None;
+    let mut cycle_end: Option<(u64, u64)> = None;
+
+    let mut states_backup = states.clone();
+    let mut stockpile_backup = stockpile.clone();
+    let mut remaining_backup = ore_remaining;
+
+    let mut count = 10000;
+
     loop {
-        let consumed = fuel_reaction.produce(&reactions, &mut stockpile);
-        ore_remaining -= consumed;
-        cycle_consumption += consumed;
-
-        if after_one
-            .iter()
-            .all(|(k, v)| stockpile.get(k).unwrap() == v)
-        {
-            break;
-        }
-    }
-
-    let num_cycles = ore_remaining / cycle_consumption;
-
-    ore_remaining -= cycle_consumption * num_cycles;
-
-    let fuel_created = stockpile.get_mut("FUEL").unwrap();
-    *fuel_created += (*fuel_created - 1) * num_cycles;
-
-    while ore_remaining > 0 {
-        if let Some(remaining) =
-            ore_remaining.checked_sub(fuel_reaction.produce(&reactions, &mut stockpile))
-        {
+        let consumed = fuel_reaction.produce(count, &reactions, &mut stockpile);
+        if let Some(remaining) = ore_remaining.checked_sub(consumed) {
             ore_remaining = remaining;
+            if ore_remaining == 0 {
+                break;
+            }
+            states_backup = states.clone();
+            stockpile_backup = stockpile.clone();
+            remaining_backup = ore_remaining;
+            let counts = (*stockpile.get("FUEL").unwrap(), ore_remaining);
+
+            if let Some(start) = states.insert(serialize_stockpile(&stockpile), counts) {
+                if start != counts {
+                    cycle_start = Some(start);
+                    cycle_end = Some(counts);
+                }
+                break;
+            }
         } else {
-            break;
+            states = states_backup.clone();
+            stockpile = stockpile_backup.clone();
+            ore_remaining = remaining_backup;
+            count /= 2;
         }
     }
 
-    let mut fuel_created = *stockpile.get("FUEL").unwrap();
     if ore_remaining > 0 {
-        fuel_created -= 1;
+        if let (Some(cycle_start), Some(cycle_end)) = (cycle_start, cycle_end) {
+            let cycle_steps = cycle_end.0 - cycle_start.0;
+            let cycle_consumption = cycle_start.1 - cycle_end.1;
+
+            let num_cycles = ore_remaining / cycle_consumption;
+
+            ore_remaining -= cycle_consumption * num_cycles;
+
+            let fuel_created = stockpile.get_mut("FUEL").unwrap();
+            *fuel_created += cycle_steps * num_cycles;
+
+            while ore_remaining > 0 {
+                if let Some(remaining) =
+                    ore_remaining.checked_sub(fuel_reaction.produce(1, &reactions, &mut stockpile))
+                {
+                    ore_remaining = remaining;
+                } else {
+                    break;
+                }
+            }
+
+            if ore_remaining > 0 {
+                *stockpile.get_mut("FUEL").unwrap() -= 1;
+            }
+        }
     }
+
+    let fuel_created = *stockpile.get("FUEL").unwrap();
 
     println!("Part 2: {}", fuel_created);
 }
@@ -76,37 +109,57 @@ struct Reaction {
 impl Reaction {
     fn produce(
         &self,
+        count: u64,
         reactions: &HashMap<String, Reaction>,
-        stockpile: &mut HashMap<String, u64>,
+        stockpile: &mut Stockpile,
     ) -> u64 {
         let mut ore_consumed = 0;
 
+        let produced = self.output.amount * count;
+
         for input in self.inputs.iter() {
+            let mut num_needed = input.amount * count;
+
             if &input.unit == "ORE" {
-                ore_consumed += input.amount;
+                ore_consumed += num_needed;
             } else {
                 let reaction = reactions.get(&input.unit).unwrap();
 
                 let in_stockpile = stockpile.entry(input.unit.to_string()).or_default();
-                let used_from_stockpile = input.amount.min(*in_stockpile);
+
+                let used_from_stockpile = num_needed.min(*in_stockpile);
                 *in_stockpile -= used_from_stockpile;
+                num_needed -= used_from_stockpile;
 
-                let num_needed = input.amount - used_from_stockpile;
                 let reactions_needed =
-                    (num_needed as f32 / reaction.output.amount as f32).ceil() as usize;
+                    (num_needed as f64 / reaction.output.amount as f64).ceil() as u64;
 
-                for _ in 0..reactions_needed {
-                    ore_consumed += reaction.produce(reactions, stockpile);
-                }
+                ore_consumed += reaction.produce(reactions_needed, reactions, stockpile);
 
                 *stockpile.entry(input.unit.to_string()).or_default() -= num_needed;
             }
         }
 
-        *stockpile.entry(self.output.unit.to_string()).or_default() += self.output.amount;
+        *stockpile.entry(self.output.unit.to_string()).or_default() += produced;
 
         ore_consumed
     }
+}
+
+type Stockpile = HashMap<String, u64>;
+
+fn serialize_stockpile(stockpile: &Stockpile) -> String {
+    stockpile
+        .iter()
+        .filter_map(|(k, v)| {
+            if k == "FUEL" {
+                None
+            } else {
+                Some(format!("{}:{}", k, v))
+            }
+        })
+        .collect::<Vec<String>>()
+        .join(",")
 }
 
 impl FromStr for Reaction {
